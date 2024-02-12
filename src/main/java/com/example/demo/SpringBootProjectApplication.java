4c165efa533a5130a0eb946f8dbaec7268f27c5a
ChaithanyaGandham <gandham.chaithanya.ui@gmail.com>
2024-02-12 15:10:09 -0600

    spring Boot Initial, Dependency Injection and Pojo class

---
+++ src/main/java/com/example/demo/SpringBootProjectApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.example.demo.model.Customer;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		
+		ConfigurableApplicationContext context = SpringApplication.run(SpringBootProjectApplication.class, args);
+		Customer cust = context.getBean(Customer.class);
+		cust.show();
+		
+		Customer cust1 = context.getBean(Customer.class);
+		cust1.show();
+		
+	}
+
+}
